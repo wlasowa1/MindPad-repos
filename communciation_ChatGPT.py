8629efe9dc35283fb56965ca33783a3273fa33d4
wlasowa1 <wiktorialasowa5@gmail.com>
2024-12-08 12:47:36 +0100

    Generating story and audio

--- communciation_ChatGPT.py
+++ communciation_ChatGPT.py
@@ -13,9 +13,12 @@ def ChatGPT_API(prompt):
     model="gpt-4o",
     messages = [
         {"role": "system",
-        "content": "You are a bedtime storyteller that tells captivating bedtime stories with language suitable for 5 year old kids."}, 
+        "content": "You tell engaging bedtime stories in simple, age-appropriate language for 5-year-olds."}, 
         {"role": "user",
         "content": prompt}
         ],
     )
+    #max_tokens = 650 # Limit the response to 650 tokens
     return response.choices[0].message.content
+
+print(ChatGPT_API("Create a bedtime story with turtle as a main character"))
